00b311c92ea4e9cb53bd09463e6b29560208db1c
demmond1 <demmond1@nationwide.com>
2018-09-02 10:59:07 -0400

    Added Hello, World! logic

--- src/edu/cscc/Main.java
+++ src/edu/cscc/Main.java
@@ -1,8 +1,10 @@
 package edu.cscc;
 
+// David Demmon, 9/1/2018
+// Lab 2 - Hello, World! program
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello, World!");
     }
 }
